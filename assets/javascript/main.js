0e1a54e7c0c651562e5a4b1a84d77a2651b8dddd
h-pk-11 <105375102+h-pk-11@users.noreply.github.com>
2023-03-26 18:05:19 +0700

    Update functions for all buttons

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -26,60 +26,117 @@ keys.addEventListener('click', event => {
 
     if (key.dataset.type === 'number') {
         
-        if (key.dataset.key === 'decimal') {
-            if(calculator.dataset.previousKeyType === 'equal'){
-                screen.textContent += ("0" + keyValue);
-            }else{
-                if (!screenValue.includes('.')) {
+        if(key.dataset.key === "signs"){
+
+            screen.textContent = `${(0 - Number(screenValue))}`;
+
+        }else if(key.dataset.key === "percent"){
+
+            screen.textContent = `${Number(screenValue)/100}`;
+
+        }else if(key.dataset.key === "delete"){
+
+            if(screen.textContent != '0'){
+
+                screen.textContent = screenValue.slice(0,-1);
+                if(screen.textContent === ''){
+                    screen.textContent = '0';
+                }
+
+            }
+
+        }else{
+
+            if (key.dataset.key === 'decimal') {
+                if(calculator.dataset.previousKeyType === 'equal'){
+                    screen.textContent += ("0" + keyValue);
+                }else{
+                    if (!screenValue.includes('.')) {
+                        screen.textContent += keyValue;
+                    }
+                }
+                
+            } else {
+                if (screenValue === '0' 
+                || calculator.dataset.previousKeyType === 'operator'
+                || calculator.dataset.previousKeyType === 'equal') {
+                    screen.textContent = keyValue;
+                } else {
                     screen.textContent += keyValue;
                 }
             }
-            
-        } else {
-            if (screenValue === '0' 
-            || calculator.dataset.previousKeyType === 'operator'
-            || calculator.dataset.previousKeyType === 'equal') {
-                screen.textContent = keyValue;
-            } else {
-                screen.textContent += keyValue;
-            }
+    
         }
+
+        
 
     }
 
     if (key.dataset.type === 'operator') {
-        if(calculator.dataset.previousKeyType === 'number'){
-            const secondNumber = screen.textContent;
+        key.classList.add('selected');
+
+        
+        if(calculator.dataset.secondNumber === ''){
+            calculator.dataset.secondNumber = screen.textContent;
+        }else{
+            calculator.dataset.firstNumber = calculator.dataset.secondNumber;
+            calculator.dataset.secondNumber = screen.textContent;
+        }
+     
+
+
+        if(calculator.dataset.firstNumber != ''
+        && calculator.dataset.secondNumber != ''
+        && calculator.dataset.operator != ''){
+            const secondNumber = calculator.dataset.secondNumber;
             const firstNumber = calculator.dataset.firstNumber;
             const operator = calculator.dataset.operator;
             let result = '';
             result = calculate(firstNumber, operator, secondNumber);
             screen.textContent = result;
+
+            calculator.dataset.firstNumber = calculator.dataset.secondNumber;
+            calculator.dataset.secondNumber = screen.textContent;
         }
-        key.classList.add('selected');
+
+        
         calculator.dataset.operator = key.dataset.key;
-        calculator.dataset.firstNumber = screen.textContent;
+  
     }
 
     if (key.dataset.type === 'equal') {
+
+        if(calculator.dataset.secondNumber === ''){
+            calculator.dataset.secondNumber = screen.textContent;
+        }else{
+            calculator.dataset.firstNumber = calculator.dataset.secondNumber;
+            calculator.dataset.secondNumber = screen.textContent;
+        }
+
         if(calculator.dataset.previousKeyType !== 'equal'){
 
-            const secondNumber = screen.textContent;
+            const secondNumber = calculator.dataset.secondNumber;
             const firstNumber = calculator.dataset.firstNumber;
             const operator = calculator.dataset.operator;
             let result = '';
             result = calculate(firstNumber, operator, secondNumber);
             screen.textContent = result;
+
+            calculator.dataset.firstNumber = '';
+            calculator.dataset.secondNumber = '';
         }
         
     }
 
-
-
+    if(key.dataset.type === 'allClear'){
+        calculator.dataset.firstNumber = '';
+        calculator.dataset.secondNumber = '';
+        calculator.dataset.operator = '';
+        screen.textContent = '0';
+    }
 
 
     calculator.dataset.previousKeyType = key.dataset.type;
-    calculator.dataset.previousKey = key.dataset.type;
 });
 
 
@@ -94,7 +151,7 @@ function calculate(firstNumber, operator, secondNumber) {
     }
 
     if (operator === "times") {
-        result = Number(firstNumber) * Number(secondNumber);
+        result = firstNumber * secondNumber;
     }
 
     if (operator === "divide") {
@@ -106,5 +163,8 @@ function calculate(firstNumber, operator, secondNumber) {
 
     }
 
+    if(result.toString().includes('.')){
+        return result.toFixed(2);
+    }
     return result;
 }
